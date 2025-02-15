5f98e9dd975715868eeb16b945ba2b6fb378b9dc
zoey-yjj <zoeyyyjj@gmail.com>
2025-02-15 16:18:41 +0800

    implement function to get order from user via terminal

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,42 @@
 int main()
 {
     OrderBook orderbook;
-    orderbook.addOrder(OrderSide::BUY, 1.2, 3.5);
-    orderbook.addOrder(OrderSide::SELL, 3.2, 3.5);
-    orderbook.addOrder(OrderSide::BUY, 2.6, 3.5);
-    orderbook.addOrder(OrderSide::SELL, 5.6, 3.5);
-    orderbook.addOrder(OrderSide::BUY, 1.8, 3.5);
-    orderbook.addOrder(OrderSide::SELL, 4.1, 3.5);
-    std::cout << "orderbook contains: " << std::endl;
-    orderbook.printOrders();
+    while (true)
+    {
+        std::cout << "\n1. Submit Order\n2. Print Order Book\n3. Exit\n";
+        int choice;
+        std::cin >> choice;
+
+        if (choice == 1)
+        {
+            std::cout << "Side (1=BUY, 2=SELL): ";
+            int i_side;
+            std::cin >> i_side;
+            OrderSide side = (i_side == 1) ? OrderSide::BUY : OrderSide::SELL;
+
+            double price;
+            std::cout << "Price: ";
+            std::cin >> price;
+
+            double amount;
+            std::cout << "Amount: ";
+            std::cin >> amount;
+
+            orderbook.addOrder(side, price, amount);
+        }
+        else if (choice == 2)
+        {
+            orderbook.printOrders();
+        }
+        else if (choice == 3)
+        {
+            break;
+        }
+        else
+        {
+            std::cout << "Invalid input, please enter again.\n";
+        }
+    }
 
     return 0;
 }
